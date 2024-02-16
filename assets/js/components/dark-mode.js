ce07f120b58a28270810971b3de350ae5d78d8b7
Andrés Gallego <programadorgallego@gmail.com>
2024-02-15 22:17:02 -0500

    small correction Dark-theme (form)

--- assets/js/components/dark-mode.js
+++ assets/js/components/dark-mode.js
@@ -1,11 +1,13 @@
 const d = document;
 const toggle = d.getElementById("darkmode-toggle");
 const $selector = d.querySelectorAll("[data-dark]");
+const $contact_form=document.querySelector("contact__form")
 
 function darkMode(e) {
   if (e) {
     $selector.forEach((el) => el.classList.toggle("dark-mode"));
   }
+  $contact_form.classList.add("light-mode");
 }
 
 toggle.addEventListener("click", darkMode);
